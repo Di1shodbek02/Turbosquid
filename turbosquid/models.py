e6bb62b50a8fc67789285ca74d973f531311641a
Di1shodbek02 <ahmatovdilshodbek@gmail.com>
2023-10-28 12:07:32 +0500

    add file (model, serializer)

--- turbosquid/models.py
+++ turbosquid/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    parent = TreeForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Files(models.Model):
+    file = models.FileField(upload_to='pics')
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+
+
+class Product(MPTTModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- turbosquid/views.py
+++ turbosquid/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Category
+from .serializer import CategorySerializer
+
+
+class CategoryAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        category = Category.objects.all().order_by('-created_at')
+        category_serializer = CategorySerializer(category, many=True)
+
+        return Response(category_serializer.data)
