7ec28000738bfd72b6eedc25d09b3b999e11c450
Di1shodbek02 <ahmatovdilshodbek@gmail.com>
2023-10-31 13:37:23 +0500

    add file

--- turbosquid/models.py
+++ turbosquid/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from django.contrib.auth.views import get_user_model
+from django.core.validators import MinValueValidator
 
 User = get_user_model()
 
@@ -12,11 +13,6 @@ class Category(MPTTModel):
 
     def __str__(self):
         return self.title
-
-
-class Files(models.Model):
-    file = models.FileField(upload_to='pics')
-    product = models.ForeignKey("Product", on_delete=models.CASCADE)
 
 
 class Product(MPTTModel):
@@ -35,3 +31,32 @@ class ShoppingCart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
+class Image(models.Model):
+    image = models.URLField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Files(models.Model):
+    file = models.URLField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)], default=1
+    )
+    creates_at = models.DateTimeField(auto_now_add=True)
+
+
+class ProductLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
