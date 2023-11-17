1228bf13d4e562bf65dd176058f4e0f494db4f99
Di1shodbek02 <ahmatovdilshodbek@gmail.com>
2023-11-17 15:52:33 +0500

    add file

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('logout', LogoutAPIView.as_view(), name='logout'),
     path('user-info', UserInfoAPIView.as_view(), name='user_info'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
-
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
